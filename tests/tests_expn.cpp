e4bb809dd2741de2de6ed3194a7cb3d7e31d23a0
Khushi Agrawal <khushiagrawal411@gmail.com>
2022-07-03 20:44:34 +0530

    add more tests

--- tests/tests_expn.cpp
+++ tests/tests_expn.cpp
@@ -6,6 +6,30 @@ TEST(expnTest, test1) {
     EXPECT_EQ (expn(11, 4), 0.00128137);
 }
 
+
 TEST(expnTest, test2) {
-    EXPECT_EQ (expn(1, 0), inf);
+    EXPECT_EQ (expn(-1, 1), 0/0.0); // returns nan
+}
+
+
+TEST(expnTest, test3) {
+    EXPECT_EQ (expn(1, 0), 1./0.);
+}
+
+
+TEST(expnTest, test4) {
+    EXPECT_EQ (expn(0, 2), 0.0676676);
+}
+
+
+TEST(expnTest, test5) {
+    double k[] = {1, 2, 3, 4};
+    EXPECT_EQ (expn(10, *k), 0.036394);
+}
+
+
+TEST(expnTest, test6) {
+    double k[] = {1, 2, 3, 4};
+    int a[] = {13, 12, 4, 4};
+    EXPECT_EQ (expn(*k, *a), 1.62187e-07);
 }
